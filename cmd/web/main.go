bf95901389ed5600cd87e50b8e3c804138c82e05
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-12-08 15:10:44 +0700

    centralize error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,10 +4,12 @@ import (
 	"encoding/gob"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/FrizkyErlang/bookings/internal/config"
 	"github.com/FrizkyErlang/bookings/internal/handlers"
+	"github.com/FrizkyErlang/bookings/internal/helpers"
 	"github.com/FrizkyErlang/bookings/internal/models"
 	"github.com/FrizkyErlang/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
@@ -17,6 +19,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -42,6 +46,12 @@ func run() error {
 	// change this true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -63,6 +73,7 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
