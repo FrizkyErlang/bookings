2a03de16448de4bf99b7332dcab59064213969ec
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2023-12-29 14:52:32 +0700

    develop SearchAvailabilityByDates

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -48,7 +48,8 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 
 	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id,
 			created_at, updated_at, restriction_id)
-			values ($1, $2, $3, $4, $5, $6, $7)`
+			values ($1, $2, $3, $4, $5, $6, $7)
+			;`
 
 	_, err := m.DB.ExecContext(ctx, stmt,
 		r.StartDate,
@@ -65,4 +66,36 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	}
 
 	return nil
+}
+
+// SearchAvailabilityByDates search room availability by dates in database for roomID
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `
+			select count(id)
+			from room_restrictions  
+			where 
+				room_id = $1
+				and $2 < end_date and $3 > start_date 
+			;`
+
+	row := m.DB.QueryRowContext(ctx, query,
+		roomID,
+		start,
+		end,
+	)
+	err := row.Scan(&numRows)
+
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
 }
