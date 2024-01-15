fff1c4308683102ac6847e5d7406cdd4331bd150
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2024-01-15 14:43:53 +0700

    develop auth middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/FrizkyErlang/bookings/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -31,4 +32,17 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and save the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+// Auth check for protected route is user log in or not
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticate(r) {
+			session.Put(r.Context(), "error", "Log in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
