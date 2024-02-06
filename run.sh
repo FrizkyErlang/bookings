40dbe755c0164cbb95f53853e5f557906a35a6f2
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2024-02-06 16:07:48 +0700

    change app to use cli flags

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-go build -o bookings cmd/web/*.go && ./bookings
+go build -o bookings cmd/web/*.go 
+./bookings -dbname=bookings -dbuser=postgres -dbpass=Frizky12 -cache=false -production=false
