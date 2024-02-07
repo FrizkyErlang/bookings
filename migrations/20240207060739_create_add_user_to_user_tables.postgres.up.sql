d996be971402002e67a7c28fe8c345fec0ce3b82
Fauzan.Erlangga <muhamad.erlangga@ibm-jti.com>
2024-02-07 13:17:38 +0700

    seeding admin user migrations

---
+++ migrations/20240207060739_create_add_user_to_user_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Fauzan','Rizky','admin@admin.com','$2a$12$mOj7SfTm3vMn4XRnnE.3Pe3JosydHN5JRI5ScreIwtLh/Y6gTrxwi',3,'2020-12-04 00:00:00','2020-12-04 00:00:00');
